15c6589e3006f599316120bbf1bece97f4bf0e30
GabrielRosaOliveira <gabriel12rosaoliveira@gmail.com>
2022-10-08 09:38:28 -0300

    iniciei exercicio 05, criei um modal usando reference

---
+++ Exercicio5/Exercicio5/Tela02/Tela02.swift
@@ -0,0 +1,21 @@
+//
+//  Tela02.swift
+//  Exercicio5
+//
+//  Created by Gabriel on 08/10/22.
+//
+
+import UIKit
+
+class Tela02: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+ 
+
+}
