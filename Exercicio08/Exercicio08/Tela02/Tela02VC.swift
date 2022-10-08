d851870fa12ddd90c0792e2d46e536954c005a32
GabrielRosaOliveira <gabriel12rosaoliveira@gmail.com>
2022-10-08 10:40:58 -0300

    iniciei exercicio 08, exibi uma NavigationController 100% programatica

---
+++ Exercicio08/Exercicio08/Tela02/Tela02VC.swift
@@ -0,0 +1,19 @@
+//
+//  Tela02VC.swift
+//  Exercicio08
+//
+//  Created by Gabriel on 08/10/22.
+//
+
+import UIKit
+
+class Tela02VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
