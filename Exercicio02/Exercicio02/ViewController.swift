5d85d713e1d1d666381db79660cc8d8fb33aad0b
GabrielRosaOliveira <gabriel12rosaoliveira@gmail.com>
2022-10-08 09:16:16 -0300

    iniciei exercicio 02, criei um modal com segue sendo referenciado pela controller

---
+++ Exercicio02/Exercicio02/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Exercicio02
+//
+//  Created by Gabriel on 08/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func tappedScreen02Button(_ sender: UIButton) {
+        performSegue(withIdentifier: "tela02", sender: nil)
+    }
+}
+
